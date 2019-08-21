1a0a92257593ae8db77f78553cd6a116c2897901
Binh Nguyen <kid.uit.1412@gmail.com>
2019-08-21 14:50:01 +0700

    [ADD] add module button collapse

---
+++ xx_be_theme_collapse/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Xao Xao Digital CO.,LTD. See LICENSE file for full copyright and licensing details.
+{
+    'name': "Button Collapse Left Panel - Xao Xao Digital",
+    'version': '1.0',
+    'summary': 'Xao Xao Backend Theme Button Collapse Left Panel',
+    'description': """
+The module which add new special button to collapse left sidebar
+    """,
+    'author': "Xao Xao Digital CO.,LTD",
+    'website': "https://www.xaoxao.vn",
+    "category": "Themes/Backend",
+    'depends': ['backend_theme_v11'],
+    'data': [
+        # Security
+        # Data
+        # Views
+        # Views Template
+        'views/assets.xml',
+        'views/templates.xml',
+        # Menu
+    ],
+    'images': [],
+    'license': 'LGPL-3',
+    'application': True,
+}
